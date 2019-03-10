e45f00e6b88b60b08c4993c5e118b596ed296896
Halldor Valur Stefansson <doristef@gmail.com>
2019-03-10 13:44:05 +0100

    Added filter search, styled elements

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <recipes />
   </div>
 </template>
 
 <script>
-
 import recipes from './components/recipes.vue'
 
 export default {
@@ -26,4 +24,32 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.container {
+  max-width: 1000px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.input {
+  display: block;
+  margin: 50px auto;  
+  width: 250px;
+  height: 20px;
+  border: 1px solid #333;
+  border-radius: 10px;
+  padding: 5px;
+}
+
+.recipe {
+  display: inline-block;
+  margin: 50px 25px;
+  padding-bottom: 25px;
+  width: 450px;
+  border-bottom: 2px solid #333;
+}
+
+.recipe a, .recipe a:active, .recipe a:visited { color: #333;text-decoration: none; }
+.recipe a:hover { color: #fd6a04; text-decoration: underline;}
+.recipe img { max-width: 150px; width: 40%; max-height: 150px; height: 40%; border-radius: 15px; border: 1px solid #333;}
 </style>
--- src/components/recipes.vue
+++ src/components/recipes.vue
@@ -1,35 +1,50 @@
 <template>
-<div>
-  <input type="text" />
-  <button type="button"> Search </button>
-  <article v-for="recipe in recipes" :key="recipe.title">
-      <h1>{{ recipe.title }}</h1>
-      <a :href="recipe.href" :title="recipe.title"> <img :src="recipe.thumbnail" /> </a>
-      <p>{{ recipe.ingredients }}</p>
-  </article>
-</div>
+  <div class="container">
+
+    <input class="input" type="text" v-model="search" placeholder="Search for a recipe" />
+
+    <div class="recipe" v-for="recipe in getRecipe" :key="recipe.title">
+      <a :href="recipe.href" :title="recipe.title"> 
+        <img :src="recipe.thumbnail" /> 
+        <h1>{{ recipe.title }}</h1>
+      </a>
+        <p><h4>Ingredients: </h4> {{ recipe.ingredients }}</p>
+    </div>
+  
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
-const url = "https://cors-anywhere.herokuapp.com/"; // Needed for Unblocking Cross-Origin request
-const apiUrl = "http://www.recipepuppy.com/api/";
+const corsURL = "https://cors-anywhere.herokuapp.com/"; // Needed for Unblocking Cross-Origin request
+const apiURL = "http://www.recipepuppy.com/api/"; // API to fetch from
 
 export default {
   data() {
     return {
       recipes: [],
-      errors: []
+      errors: [],
+      search: ''
+    }
+  },
+
+  computed: {
+    getRecipe() {
+      return this.recipes.filter((recipe) => {
+        return recipe.title.toLowerCase()
+        .indexOf(this.search.toLowerCase()) > -1;
+      });
     }
   },
 
   created() {
-    axios.get(url + apiUrl)
+    axios.get(corsURL + apiURL)
     .then(response => { this.recipes = response.data.results })
     .catch(e => {
       this.errors.push(e)
     })
-
   }
+
+
 }
 </script>
